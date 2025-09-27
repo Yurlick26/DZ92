652d5d4a5812da69b6a449e0bde41f165041f77e
Vladyslav <vladurlov17@gmail.com>
2025-09-27 09:48:44 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        
+        StringBuilder report = new StringBuilder();
+
+      
+        report.AppendLine("=== Text report ===");
+        report.AppendLine("Date: " + DateTime.Now.ToShortDateString());
+        report.AppendLine("List of events:");
+
+        while (true)
+        {
+            Console.Write("Enter the event (or 'exit' to complete): ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "exit") break;
+
+            report.AppendLine("- " + input);
+        }
+
+        
+        Console.WriteLine("\nYour report:");
+        Console.WriteLine(report.ToString());
+    }
+}
